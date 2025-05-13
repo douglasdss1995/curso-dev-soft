d4039fcd41c37a5e19996912e690fc14b2f29515
Lígia Nascimento <ligianascimento035@gmail.com>
2025-05-13 11:33:56 -0400

    Aula 12 - 13/05/25

---
+++ javascript/variaveis/tipos-de-referencias.js
@@ -0,0 +1,14 @@
+// Object/Objeto
+var objeto = {
+    nome: "Maria",
+    sobrenome:"Silva",
+}
+console.log(objeto);
+
+// Array/Lista
+var array = [1, 2, 3];
+console.log(array);
+
+// Date/Datas
+let data = new Date()
+console.log(data);
---
+++ javascript/variaveis/variaveis.js
@@ -0,0 +1,21 @@
+// String
+var nome = "Lígia";
+console.log(nome);
+
+// Undefined
+var indefinido;
+console.log();
+
+// Boolean
+var ativo = true;
+var inativo = false;
+console.log(ativo);
+console.log(inativo);
+
+// Numero
+var numero = 1
+console.log(numero);
+
+// Numero grande 
+var numero_grande = 90000000000000000000;
+console.log(numero_grande);
