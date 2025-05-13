33a8d46a6120c0dc4d9eb66492ba3f1edaa1147c
Lígia Nascimento <ligianascimento035@gmail.com>
2025-05-13 11:36:08 -0400

    aula dia13/05/2025

---
+++ javascript/variaveis/variaveis.js
@@ -0,0 +1,16 @@
+var nome = "Douglas";
+console.log(nome);
+
+var ativo= true;
+var inativo= false;
+console.log(ativo);
+console.log(inativo);
+
+var indefinido;
+console.log(indefinido);
+
+var numero =1;
+console.log(numero);
+
+var numero_grande = 90000000000n;
+console.log(numero_grande);
