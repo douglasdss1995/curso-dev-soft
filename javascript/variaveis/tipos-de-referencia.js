76f409c047f63ddbdffc0355b9f00709931072c0
jander <jrodrigues161@gmail.com>
2025-05-13 11:36:59 -0400

    commit aula 13/05/2025 walderson

---
+++ javascript/variaveis/tipos-de-referencia.js
@@ -0,0 +1,13 @@
+var objeto ={
+    nome: "Maria",
+    sobrenome:"Silva",
+};
+console.log(objeto);
+
+//array/lista
+var array = [1, 2, 3, 4, 5];
+console.log(array);
+
+// date/data
+let data = new Date()
+console.log(data);
---
+++ javascript/variaveis/variaveis.js
@@ -0,0 +1,23 @@
+
+
+
+var nome = "Douglas";
+console.log(nome);
+
+//boolean
+var ativo = "True";
+var inativo = "false";
+console.log(ativo);
+console.log(inativo);
+
+//undefined
+var indefinido;
+console.log(indefinido);
+
+//Número
+var numero=1;
+console.log(numero);
+
+//numero_grande
+var numero_grande=90000000000000;
+console.log(numero_grande);
