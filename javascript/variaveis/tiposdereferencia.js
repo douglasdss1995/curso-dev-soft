a84b63aa411b212738022f389c3585c7d2659c24
Rita Costa <ritacosta565@gmail.com>
2025-05-13 11:35:23 -0400

    Aula12-2025-05-13

---
+++ javascript/variaveis/tiposdereferencia.js
@@ -0,0 +1,14 @@
+var objeto = {
+    nome: "Maria",
+    sobrenome: "Silva",
+}
+
+console.log(objeto);
+
+//array/Lista
+
+var array = [1, 2, 3];
+console.log(array);
+
+let data = new Date()
+console.log(data);
---
+++ javascript/variaveis/variaveis.js
@@ -0,0 +1,21 @@
+//String
+var nome= "Douglas";
+console.log(nome);
+
+//Boolean
+var ativo = true;
+var inativo = false;
+console.log(ativo);
+console.log(inativo);
+
+//Undefined
+var indefinido;
+console.log(indefinido);
+
+//Numero
+var numero = 1
+console.log(numero);
+
+//Numero grande
+var numero_grande = 9000000000000000000000n;
+console.log(numero_grande);
