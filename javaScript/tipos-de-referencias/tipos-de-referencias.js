cacfb44b8925c7406831a7f0d3daa6f0245d375c
israel malheiros <israelmalhirosoliveira@gmail.com>
2025-05-13 11:34:33 -0400

    Aula JavaScript 2025/05/13

---
+++ javaScript/tipos-de-referencias/tipos-de-referencias.js
@@ -0,0 +1,14 @@
+//Object/Objeto
+var Objeto = { 
+    nome: "Maria",
+    sobrenome: "Silva",
+}
+console.log(Objeto);
+
+//Array/Lista
+var Array = [1, 2, 3];
+console.log(Array)
+
+//Date/Datas
+let data = new Date();
+console.log(data);
---
+++ javaScript/variaveis/variaveis.js
@@ -0,0 +1,21 @@
+//String
+var nome = "Douglas";
+console.log(nome);
+
+//Booleano
+var ativo = true;
+var inativo = false;
+console.log(ativo);
+console.log(inativo);
+
+//Undefined
+var indefinido;
+console.log(indefinido);
+
+//Número
+var numero = 1;
+console.log(numero);
+
+//Número grande
+var numero_grande = 90000000000000;
+console.log(numero_grande);
