5b1982272f5dfa214839a9f6ab6fde9b6ab476b4
israel malheiros <israelmalheirosoliveira@gmail.com>
2025-05-13 11:39:10 -0400

    aula 12 13/05/2025

---
+++ javascript/variaveis/variaveis.js
@@ -0,0 +1,19 @@
+//string
+
+var nome ="Israel";
+console.log(nome);
+
+var ativo = true;
+var inativo = false;
+console.log(ativo);
+console.log(inativo);
+
+var indefinido;
+console.log(indefinido);
+
+var numero = 1;
+console.log(numero);
+
+
+var numero_grande = 900000000000n;
+console.log(numero_grande);
