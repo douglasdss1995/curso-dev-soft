4e17c82ba716aedafbebcd572d27cc62436473fd
Silvia Andrade <silviacrisandrade@gmail.com>
2025-05-13 11:33:45 -0400

    Aula 13-05-2025

---
+++ javascript/variaveis/variaveis.js
@@ -0,0 +1,17 @@
+//string
+var nome ="Douglas";
+console.log(nome);
+
+var ativo = true;
+var inativo = false;
+console.log(ativo);
+console.log(inativo);
+
+var indefinido;
+console.log(indefinido);
+
+var numero = 1;
+console.log(numero);
+
+var numero_grande = 900000000000000000000000n;
+console.log(numero_grande);
