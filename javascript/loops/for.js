d0d8d4074f0dcc7ad64d45e6645791937ba69410
Silvia Andrade <silviacrisandrade@gmail.com>
2025-05-15 09:42:53 -0400

    Aula 15-05-2025

--- javascript/loops/for.js
+++ javascript/loops/for.js
@@ -1,19 +1,34 @@
-let str = " ";
+let str = "";
 
-for (let i = 0; i < 9; i++) {
-str = str +i;
-console.log(str);
+for (let i = 0; i < 10; i++) {
+    // str = str + i;
+    str += i;
+    console.log(str);
 }
 
 console.log(str);
 
 let int = 1;
-for(let i = 0; i < 9; i++) {
-int = int+i;
-console.log(int);
-};
 
-const array = ["a","b","c"];
-for(const element of array1){
-    console.log(element)
-};
+for (let i = 0; i < 9; i++) {
+    int = int + i;
+    console.log(int);
+}
+
+console.log(int);
+
+// Declara um array com os números de 1 a 5
+const array1 = ["1", "2", "3", "4", "5"];
+
+for (const element of array1) {
+    console.log(element);
+}
+
+array1.forEach(
+    element => {
+        console.log(element);
+        console.log(element);
+        console.log(element);
+        console.log(element);
+    }
+);
