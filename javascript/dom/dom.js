1f666880db8a5d957ea46be3ebbca338ad1d3557
jonasifam <jonas.ifam@gmail.com>
2025-05-16 08:22:38 -0400

    Aula - 15-05-2025

--- javascript/dom/dom.js
+++ javascript/dom/dom.js
@@ -16,3 +16,39 @@ const elementoPorQuery = document.querySelector('.paragrafo');
 //retorna todos os elementos que correspondem ao selector CSS
 //utiliza os mesmos padroes dos selectores CSS
 const elementoPorQueryAll = document.querySelectorAll('#titulo');
+
+
+let elemento = document.getElementById('titleDiv');
+
+// textContent
+let text = elemento.textContent; // obter o valor
+elemento.textContent = "Novo texto"; //definir valor
+
+//innerHTML
+text = elemento.innerHTML; //obter o valor
+elemento.innerHTML = "<h1 id='titleH1'>Novo Texto</h1>"; //definir valor utilizando HTML
+
+//style
+elemento = document.getElementById('titleH1');
+elemento.style.color = 'blue';
+elemento.style.backgroundColor = 'red';
+elemento.style.fontSize = '3rem';
+
+//classList
+
+//utiliza o primeiro elemento com a classe paragrafo
+elemento = document.getElementsByClassName('paragraph')[0];
+elemento.classList.add('text-warning');
+elemento.classList.remove('text-warning');
+
+elemento = document.getElementsByClassName('paragraph');
+for (let i = 0; i < elemento.length; i++) {
+    elemento[i].classList.toggle('text-warning');
+}
+
+elemento = document.getElementById('inputNumber');
+attr = elemento.getAttribute('type');
+console.log(attr);
+
+elemento.setAttribute('type', 'number');
+elemento.removeAttribute('placeholder');
---
+++ javascript/eventos/eventos.js
@@ -0,0 +1,21 @@
+var divResult = document.getElementById("divResult");
+var btnButton = document.getElementById("btnButton");
+
+function click() {
+    divResult.textContent = "Você clicou no botão!";
+}
+
+function mouseOver() {
+    divResult.textContent = "Você passou o mouse por cima!";
+}
+
+function nouseOut() {
+    divResult.textContent = "Você retirou o mouse!";
+    setTimeout(() => {
+        divResult.textContent = null;
+    }, 3000)
+}
+
+btnButton.addEventListener("click", click);
+btnButton.addEventListener("mouseover", mouseOver);
+btnButton.addEventListener("mouseout", mouseOut);
