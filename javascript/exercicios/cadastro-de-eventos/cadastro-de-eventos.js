131322badd15989093a2657a1066ca4a55c3d1b2
Lígia Nascimento <ligianascimento035@gmail.com>
2025-05-16 11:33:10 -0400

    Aula JavaScript 16/05/2025

---
+++ javascript/exercicios/cadastro-de-eventos/cadastro-de-eventos.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("form");
+const tableBody = document.getElementById("tableBody");
+
+// Função para adicionar uma nova linha na tabela
+function addUser(nome, email, telefone) {
+  const tableRow = document.createElement("tr");
+  tableRow.innerHTML = `<td>${nome}</td> <td>${email}</td> <td>${telefone}</td>`;
+  tableBody.appendChild(tableRow);
+}
+
+/**
+ * Função para validar o formulário
+ * @param {Event} event - O evento de submit do formulário
+ */
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // Previne o envio do formulário
+  event.stopPropagation(); // Para evitar que o evento de submit se propague e atualize a página
+
+  // Valida o formulário
+  if (!form.checkValidity()) {
+    form.classList.add("was-validated");
+    return;
+  }
+
+  // Captura os valores dos inputs
+  // Utiliza o método trim() para remover espaços em branco
+  const nome = form.inputNome.value.trim();
+  const email = form.inputEmail.value.trim();
+  const telefone = form.inputTelefone.value.trim();
+  // Adiciona à tabela
+  addUser(nome, email, telefone);
+  // Limpa o formulário e feedback visual
+  form.reset();
+  form.classList.remove("was-validated");
+});
