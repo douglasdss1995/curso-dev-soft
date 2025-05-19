440009f7f07818af9e644c614b506b35b16bd9a9
douglas.silva <douglas.silva@fpf.br>
2025-05-19 14:42:39 -0400

    Aula 16 - 19/05/2025

---
+++ angular/hello-world/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {HelloWorldComponent} from './hello-world/hello-world.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [HelloWorldComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'hello-world';
+}
