5e1c85c78c43ad4f901b76668cbca8a0e16cfd5b
jonasifam <jonas.ifam@gmail.com>
2025-05-20 11:26:31 -0400

    Aula - 20/05/2025

---
+++ angular/hello-world/src/app/data-binding/event-binding/event-binding.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  imports: [],
+  templateUrl: './event-binding.component.html',
+  styleUrl: './event-binding.component.css'
+})
+export class EventBindingComponent {
+  message: string = 'Clique no botão para mudar esta mensagem';
+
+  changeMessage(): void {
+    this.message = 'Você clicou no botão';
+  }
+}
---
+++ angular/hello-world/src/app/data-binding/interpolation/interpolation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolation',
+  imports: [],
+  templateUrl: './interpolation.component.html',
+  styleUrl: './interpolation.component.css'
+})
+export class InterpolationComponent {
+  ola:string = "Olá";
+}
