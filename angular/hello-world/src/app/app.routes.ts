09dfd448446f36d2bdc2862e82f3141e49b9b62d
douglas.silva <douglas.silva@fpf.br>
2025-05-23 08:37:27 -0400

    Aula 20 - 22/05/2025

--- angular/hello-world/src/app/app.routes.ts
+++ angular/hello-world/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import {Routes} from '@angular/router';
 import {EventBindingComponent} from './data-binding/event-binding/event-binding.component';
 import {InterpolationComponent} from './data-binding/interpolation/interpolation.component';
 import {AttributeComponent} from './directives/attribute/attribute.component';
-import {AmIOldComponent} from './directives/am-i-old/am-i-old.component';
+import {AmIOldComponent} from './directives/attribute/am-i-old/am-i-old.component';
 
 export const routes: Routes = [
   {path: 'event_binding', component: EventBindingComponent},
