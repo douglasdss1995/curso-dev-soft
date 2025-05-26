e5558d7d21b926b24ee69b4df082b1beb7e5624a
douglas.silva <douglas.silva@fpf.br>
2025-05-26 08:52:47 -0400

    Aula 22 - 26/05/2025

--- angular/hello-world/src/app/app.routes.ts
+++ angular/hello-world/src/app/app.routes.ts
@@ -4,11 +4,16 @@ import {InterpolationComponent} from './data-binding/interpolation/interpolation
 import {AmIOldComponent} from './directives/structural/am-i-old/am-i-old.component';
 import {AtributeComponent} from './directives/atribute/atribute.component';
 import {PipesComponent} from './pipes/pipes.component';
-import {PresentationComponent} from './angular-material/presentation/presentation.component';
 import {DatapickerComponent} from './angular-material/datapicker/datapicker.component';
 import {SliderComponent} from './angular-material/slider/slider.component';
 import {FormFieldComponent} from './angular-material/form-field/form-field.component';
 import {SidenavComponent} from './angular-material/sidenav/sidenav.component';
+import {ButtonComponent} from './angular-material/button/button.component';
+import {TogglesComponent} from './angular-material/toggles/toggles.component';
+import {TreeComponent} from './angular-material/tree/tree.component';
+import {
+  DialogOverviewExampleComponent
+} from './angular-material/dialog-overview-example/dialog-overview-example.component';
 
 export const routes: Routes = [
   {path: 'event_binding', component: EventBindingComponent},
@@ -16,9 +21,12 @@ export const routes: Routes = [
   {path: 'diretive_attribute', component: AtributeComponent},
   {path: 'am_i_old', component: AmIOldComponent},
   {path: 'pipes', component: PipesComponent},
-  {path: 'am_button', component: PresentationComponent},
   {path: 'am_datepicker', component: DatapickerComponent},
   {path: 'am_slider', component: SliderComponent},
   {path: 'am_form_field', component: FormFieldComponent},
   {path: 'am_sidenav', component: SidenavComponent},
+  {path: 'am_button', component: ButtonComponent},
+  {path: 'am_toggles', component: TogglesComponent},
+  {path: 'am_tree', component: TreeComponent},
+  {path: 'am_dialog', component: DialogOverviewExampleComponent},
 ];
